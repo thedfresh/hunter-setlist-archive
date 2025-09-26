28412c2925a51b8bad2af9fb0d009b3d44f68fda
Douglas Aldridge <dfresh@gmail.com>
2025-09-25 20:35:28 -0400

    new home page

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,16 @@ export default function Home() {
           Hunter Setlist Archive
         </h1>
         <p className="text-center text-lg">
-          Robert Hunter performance database - v2.0 rebuild in progress (new updates coming soon!)
+          Robert Hunter performance database - v2.0 rebuild in progress, featuring...
+        </p>
+          <ul className="flex flex-col items-center justify-center">
+            <li>New user interface</li>
+            <li>Hundreds of updates and corrections</li>
+            <li>Annotated and searchable setlists</li>
+            <li>and much more!</li>
+          </ul>
+        <p className="text-center text-lg mt-8">
+          Email <a href="mailto:dfresh@gmail.com">dfresh@gmail.com</a> with inquiries or updates!
         </p>
       </div>
     </main>
