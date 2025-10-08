2aa85b252cbe3c77b8001c1f70b7c62b952ae9d2
Douglas Aldridge <dfresh@gmail.com>
2025-10-08 10:09:12 -0400

    typo fix

--- app/api/admin/events/[id]/sets/route.ts
+++ app/api/admin/events/[id]/sets/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
-\
+
 export async function POST(req: Request, { params }: { params: Promise<{ id: string }> }) {
   const { id } = await params;
   const eventId = Number(id);
