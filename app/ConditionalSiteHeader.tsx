6d332d9b7193b9e58cc4935488873186a6836e05
Douglas Aldridge <dfresh@gmail.com>
2025-10-08 20:47:41 -0400

    site launch!

--- app/ConditionalSiteHeader.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import { usePathname } from 'next/navigation';
-import SiteHeader from '@/components/ui/SiteHeader';
-
-export default function ConditionalSiteHeader() {
-  const pathname = usePathname();
-  if (pathname === '/') return null;
-  return <SiteHeader />;
-}
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
 import './globals.css'
-import ConditionalSiteHeader from './ConditionalSiteHeader';
+import SiteHeader from '../components/ui/SiteHeader';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-  <ConditionalSiteHeader />
+        <SiteHeader />
         {children}
       </body>
     </html>
