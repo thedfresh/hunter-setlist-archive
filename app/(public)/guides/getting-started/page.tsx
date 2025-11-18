361fb3e77c772418a006e5da69d54863ef4afbfa
Douglas Aldridge <dfresh@gmail.com>
2025-11-18 14:40:03 -0500

    content update

--- app/(public)/guides/getting-started/page.tsx
+++ app/(public)/guides/getting-started/page.tsx
@@ -17,13 +17,13 @@ export default function GettingStartedPage() {
                     Coming soon - an overview of Hunter's career as a musician, published author and lyricist.
                 </p>
                 <p className="mb-4">
-                    For now, the best introduction to Hunter's career is the Good Ol' Grateful Deadcast <ExternalLink href="https://www.dead.net/deadcast/bonus-keys-rain-celebrating-robert-hunters-80th">episode on Hunter</ExternalLink>, which offers a full hour on Hunter's career with and without the Dead.
-                </p>
-                <p className="mb-4">
                     Hunter's early career with the Liberty Hill Aristocrats, who later became Roadhog, is well documented on Corry Arnold's <ExternalLink href="https://lostlivedead.blogspot.com/2010/11/robert-hunter-and-roadhog-performance.html">Lost Live Dead Roadhog</ExternalLink> blog entry.  Additional material from this era, including fantastic interviews with some of the participants, can be found on the <ExternalLink href="https://www.dead.net/deadcast/bonus-tales-great-rum-runners-50">Rum Runners 50</ExternalLink> episode of the Deadcast.  The Deadcast also covered <ExternalLink href="https://www.dead.net/deadcast/bonus-tiger-rose-50">the recording of Tiger Rose</ExternalLink> - both episodes are essential listnening for those curious about Hunter's solo work.
                 </p>
                 <p className="mb-4">
                     Hunter's subsequent career with Comfort is documented in a <ExternalLink href="https://lostlivedead.blogspot.com/2009/07/robert-hunter-and-comfort-performing.html">subsequent LLD post</ExternalLink>.  And the Dinosaurs era is covered on LLD's sister blog, <ExternalLink href="https://hooterollin.blogspot.com/2016/09/dinosaurs-with-robert-hunter-1982-84.html">Hooteroolin' Around</ExternalLink>.
+                </p>
+                <p className="mb-4">
+                    Finally, the Good Ol' Grateful Deadcast <ExternalLink href="https://www.dead.net/deadcast/bonus-keys-rain-celebrating-robert-hunters-80th">episode on Hunter</ExternalLink> offers a deep dive on Hunter's poetry and other writings beyong Grateful Dead lyrics.
                 </p>
                 <p>
                     More to come!
